f7139d09d8f7a4e9fd6dfc1e526ab593ffab7ddd
qkkcoolmax <371571945@qq.com>
2015-06-30 23:50:01 +0800

    sds

--- myakka/src/akkatest/FirstActor.java
+++ myakka/src/akkatest/FirstActor.java
@@ -20,8 +20,10 @@ public class FirstActor extends UntypedActor {
 
 	public static void main(String[] args) {
 		ActorSystem system = ActorSystem.create("mySystem");
+		
 		ActorRef actor1 = system.actorOf(Props.create(FirstActor.class),
 				"actor");
+		//actorOf 创建actor actorFor获取已经存在的actor
 		actor1.tell("hahahah", ActorRef.noSender());
 		system.shutdown();
 
